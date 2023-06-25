4a9b7901c0f4e2833d07ab042d1ca9c057d6fe7f
yusufgltc <ygultac@hotmail.com>
2023-06-25 13:27:02 +0200

    Add my solution of Fibonacci Sequence

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("junit:junit:4.13.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
