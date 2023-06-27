cd3aba3b47e2e5478cfd0952a8f2340398218264
yusufgltc <ygultac@hotmail.com>
2023-06-27 23:03:16 +0200

    Refactor my approach

--- java/src/main/java/org/example/sudoku_validator/SudokuValidator.java
+++ java/src/main/java/org/example/sudoku_validator/SudokuValidator.java
@@ -5,51 +5,54 @@ import java.util.Set;
 
 public class SudokuValidator {
     public static boolean isValidSudoku(char[][] board) {
-        // Check rows
+        if (board.length == 0) {
+            return false;
+        }
+
+        //rows
         for (int row = 0; row < 9; row++) {
-            if (isValid(board[row])) {
+            if (hasDuplicate(board[row])) {
                 return false;
             }
         }
 
-        // Check columns
+        //columns
         for (int col = 0; col < 9; col++) {
             char[] column = new char[9];
             for (int row = 0; row < 9; row++) {
                 column[row] = board[row][col];
             }
-            if (isValid(column)) {
+            if (hasDuplicate(column)) {
                 return false;
             }
         }
 
-        // Check sub-boxes
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        //sub-boxes
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
                 char[] subBox = new char[9];
                 int index = 0;
-                for (int row = i * 3; row < (i + 1) * 3; row++) {
-                    for (int col = j * 3; col < (j + 1) * 3; col++) {
+                for (int row = j * 3; row < (j + 1) * 3; row++) {
+                    for (int col = k * 3; col < (k + 1) * 3; col++) {
                         subBox[index++] = board[row][col];
                     }
                 }
-                if (isValid(subBox)) {
+                if (hasDuplicate(subBox)) {
                     return false;
                 }
             }
         }
-
         return true;
     }
 
-    private static boolean isValid(char[] nums) {
+    private static boolean hasDuplicate(char[] chars) {
         Set<Character> digits = new HashSet<>();
-        for (char num : nums) {
-            if (num != '.') {
-                if (digits.contains(num)) {
+        for (char chr : chars) {
+            if (chr != '.') {
+                if (digits.contains(chr)) {
                     return true;
                 }
-                digits.add(num);
+                digits.add(chr);
             }
         }
         return false;
