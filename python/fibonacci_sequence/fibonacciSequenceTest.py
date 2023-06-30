cf41862a3f7a82fd4f364b62c2890ba1cb4dd6e3
OLGUN ONAL <olgunonalgit@gmail.com>
2023-06-30 19:51:22 +0200

    adding my solution in python

---
+++ python/fibonacci_sequence/fibonacciSequenceTest.py
@@ -0,0 +1,18 @@
+import unittest
+from fibonacciSequence import Fibonacci
+
+
+class FibonacciSequenceTest(unittest.TestCase):
+
+    # def test_initial_check(self):
+    #     self.assertEqual(Fibonacci.fibonacci(0), [])
+
+    def test_valid_input(self):
+        self.assertEqual(Fibonacci.fibonacci(10), [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+
+    def test_invalid_result(self):
+        self.assertNotEqual(Fibonacci.fibonacci(5), [0, 1, 1, 2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
