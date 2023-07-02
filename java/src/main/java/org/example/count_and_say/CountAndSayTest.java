14c1fbd99fba5f93f29a753612113335a23f17d9
yusufgltc <ygultac@hotmail.com>
2023-07-02 12:57:26 +0200

    Adding 'Count and Say' with recursive method solution in java

--- java/src/main/java/org/example/count_and_say/CountAndSayTest.java
+++ java/src/main/java/org/example/count_and_say/CountAndSayTest.java
@@ -3,8 +3,6 @@ package org.example.count_and_say;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class CountAndSayTest {
     @Test
     public void valid_test() {
@@ -15,5 +13,16 @@ public class CountAndSayTest {
     @Test
     public void invalid_test() {
         Assert.assertNotEquals("1", CountAndSay.countAndSay(2));
+    }
+
+    @Test
+    public void valid_test_recursive() {
+        Assert.assertEquals("1", CountAndSay.countAndSayRecursive(1));
+        Assert.assertEquals("1211", CountAndSay.countAndSayRecursive(4));
+    }
+
+    @Test
+    public void invalid_test_recursive() {
+        Assert.assertNotEquals("1", CountAndSay.countAndSayRecursive(2));
     }
 }
