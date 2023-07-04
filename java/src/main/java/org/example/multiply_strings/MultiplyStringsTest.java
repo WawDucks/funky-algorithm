f28eefc560d69cfaa15c78e5a0010a2199793d22
yusufgltc <ygultac@hotmail.com>
2023-07-04 07:01:52 +0200

    Adding 'Multiply Strings' solution in java

---
+++ java/src/main/java/org/example/multiply_strings/MultiplyStringsTest.java
@@ -0,0 +1,18 @@
+package org.example.multiply_strings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MultiplyStringsTest {
+
+    @Test
+    public void valid_test() {
+        Assert.assertEquals("6", MultiplyStrings.multiplyStrings("2", "3"));
+        Assert.assertEquals("56088", MultiplyStrings.multiplyStrings("123", "456"));
+    }
+
+    @Test
+    public void base_test() {
+        Assert.assertEquals("121932631112635269", MultiplyStrings.multiplyStrings("123456789", "987654321"));
+    }
+}
