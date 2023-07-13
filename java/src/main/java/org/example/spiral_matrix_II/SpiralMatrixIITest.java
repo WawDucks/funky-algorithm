a96547804a54d4a3cc50b967a1836afef9a10abb
yusufgltc <ygultac@hotmail.com>
2023-07-13 12:06:34 +0200

    Adding 'Spiral Matrix II' solution in java

---
+++ java/src/main/java/org/example/spiral_matrix_II/SpiralMatrixIITest.java
@@ -0,0 +1,21 @@
+package org.example.spiral_matrix_II;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SpiralMatrixIITest {
+    @Test
+    public void valid_test() {
+        int input = 3;
+        int[][] output = new int[][]{{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
+        Assert.assertArrayEquals(output, SpiralMatrixII.generateMatrix(input));
+    }
+
+    @Test
+    public void valid_test2() {
+        int input = 1;
+        int[][] output = new int[][]{{1}};
+        Assert.assertArrayEquals(output, SpiralMatrixII.generateMatrix(input));
+    }
+
+}
